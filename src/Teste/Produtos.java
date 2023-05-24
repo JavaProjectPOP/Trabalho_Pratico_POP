a183b5947b0b7045aaeba696176758a525a02e17
Andre Carvalho <andre.filipe.carvalho@outlook.pt>
2023-05-24 01:08:24 +0100

    .
    .

--- src/Teste/Produtos.java
+++ src/Teste/Produtos.java
@@ -1,4 +1,12 @@
 package Teste;
 
 public class Produtos {
+    private String NomeProduto;
+    private String Categoria;
+    private double Preco;
+    private int Stock;
+
+    public Produtos(){
+
+    }
 }
